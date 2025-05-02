c9c9d3d37e0e11120e1387527255c46df7424bb6
ziiiiiiiiiiiiiiiiii <fve1981@gmail.com>
2025-05-02 11:14:07 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let capture;
+let graphics;
 
 function setup() {
   // 建立全螢幕畫布
@@ -9,6 +10,9 @@ function setup() {
   capture = createCapture(VIDEO);
   capture.size(windowWidth * 0.8, windowHeight * 0.8);
   capture.hide(); // 隱藏原始攝影機影像
+
+  // 建立與攝影機畫面相同大小的圖形
+  graphics = createGraphics(capture.width, capture.height);
 }
 
 function draw() {
@@ -19,15 +23,31 @@ function draw() {
   let x = (width - capture.width) / 2;
   let y = (height - capture.height) / 2;
 
+  // 更新 graphics 的內容
+  graphics.background(0); // 設定背景為黑色
+  for (let i = 0; i < capture.width; i += 20) {
+    for (let j = 0; j < capture.height; j += 20) {
+      // 從攝影機影像中取顏色
+      let col = capture.get(i, j);
+      graphics.fill(col);
+      graphics.noStroke();
+      graphics.ellipse(i + 10, j + 10, 15, 15); // 繪製圓形
+    }
+  }
+
   // 翻轉畫布以水平翻轉影像
   push();
   translate(width, 0); // 將原點移到右上角
   scale(-1, 1); // 水平翻轉畫布
   image(capture, x, y, capture.width, capture.height);
   pop();
+
+  // 在攝影機畫面上方顯示圖形
+  image (graphics, (width - graphics.width) / 2, (height - graphics.height) / 2); // 將圖形顯示在攝影機畫面上方，並留 10px 間距
 }
 
 function windowResized() {
   // 當視窗大小改變時，調整畫布大小
   resizeCanvas(windowWidth, windowHeight);
 }
+
